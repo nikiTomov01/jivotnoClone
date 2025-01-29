b99ef7dd4a5f48b89aba5fa7674da55b3ed49e54
Nik <nikolaitomov25@gmail.com>
2025-01-29 16:46:17 +0200

    added base mechancis for duel mode

--- src/duels.js
+++ src/duels.js
@@ -18,3 +18,61 @@ const player = document.getElementById("character");
 let playerHp = 100;
 let playerDmg = 10;
 let playerSpeed = 5;
+
+let currTurn = "character";
+
+let start = async function startDuel() {
+    while (monsterHp > 0) {
+        if (currTurn === "character") {
+            await playerTurn();
+            monsterHp -= 10;
+            currTurn = "monster";
+        }
+        else {
+            await monsterTurn();
+            currTurn = "character";
+        }
+    }
+}
+
+// ui stuff
+const duelBtn = document.getElementById("duel-btn");
+duelBtn.addEventListener("click", start);
+
+// players turn 
+async function playerTurn() {
+    console.log("Players turn");
+    await playAnimation("character");
+    await delay(500);
+}
+
+async function monsterTurn() {
+    console.log("monster turn");
+    await playAnimation("monster");
+    await delay(500);
+}
+
+// play animation
+async function playAnimation(character) {
+    const element = document.getElementById(character);
+    element.classList.add(`${character}-attack`);
+
+    await waitForAnimationEnd(element);
+
+    element.classList.remove(`${character}-attack`);
+
+}
+
+//helper function to wait for animation end
+function waitForAnimationEnd(element) {
+    return new Promise(resolve => {
+        element.addEventListener("animationend", function onAnimationEnd() {
+            element.removeEventListener("animationend", onAnimationEnd);
+            resolve();
+        }, { once: true});
+    })
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
