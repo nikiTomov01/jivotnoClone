bc5771809558da70672330ea900e81d17cf3666b
Nik <nikolaitomov25@gmail.com>
2025-02-04 23:58:32 +0200

    added function to generate shop window, needs body tho

--- src/mall.js
+++ src/mall.js
@@ -1,3 +1,13 @@
 import { headerInit } from "./statLoading";
 
 headerInit();
+
+const clothingStore = document.getElementById("clothing-store-mall");
+clothingStore.addEventListener("click", () => {
+    generateStoreWindow();
+})
+
+function generateStoreWindow() {
+    //code to generate a store window
+    console.log("generating store window...");
+}
